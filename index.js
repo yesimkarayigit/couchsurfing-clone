108616f517bd15adf8fdc88b5a72c692e1382342
Yeşim Karayiğit <yesim.karayigit@gmail.com>
2020-04-26 00:24:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { surferDatabase, hostDatabase } = require('./database')
 const printBookingHistory = require('./lib/print-booking-history')
 
 async function main() {
-  const yesim = surferDatabase.findByName('Yesim')
+  const yesim = await surferDatabase.findByName('Yesim')
+  const nihal = await hostDatabase.findByHostName('Nihal')
 
-  const surfers = await surferDatabase.load()
-  surfers.forEach(printBookingHistory)
+  yesim.book(nihal, 'Leipzig')
+  surferDatabase.update(yesim)
+  
+  console.log(await hostDatabase.findBy('location', 'Leipzig'))
 }
-
 main()
